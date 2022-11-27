94c3c415cebbcf8193775c58bcba006d8608b3de
Barren <xaboule@gmail.com>
2022-11-27 11:34:46 +0100

    lights

--- src/Components/Scene.jsx
+++ src/Components/Scene.jsx
@@ -77,11 +77,11 @@ const faceMap = useLoader(TextureLoader, "/headus/Colour_8k-min-min.jpg");
 
 
 // 
-useFrame(({ clock }) => {
-  const a = clock.getElapsedTime();
-  headFull.current.rotation.y = -1 + Math.sin(a * 1) * 0.3; // the value will be 0 at scene initialization and grow each frame
-  // headFull.current.group.rotation.x = 0.2 + Math.cos((a / 2) * 2) * 0.03; // the value will be 0 at scene initialization and grow each frame
-});
+// useFrame(({ clock }) => {
+//   const a = clock.getElapsedTime();
+//   headFull.current.rotation.y = -1 + Math.sin(a * 1) * 0.3; // the value will be 0 at scene initialization and grow each frame
+//   // headFull.current.group.rotation.x = 0.2 + Math.cos((a / 2) * 2) * 0.03; // the value will be 0 at scene initialization and grow each frame
+// });
 // if(headFull.current){
 //   console.log(headFull.current[2])
 // }
@@ -91,140 +91,146 @@ useFrame(({ clock }) => {
 
       //MAPS IMPORT
 
-      return (
-        <>
-          <group
-            {...props}
-            dispose={null}
-            ref={headFull}
-            position={[5, 1.5, 9]}
-            rotation={[0.2, -0.8, 0]}
-          >
-    
-            <mesh
-              geometry={nodes.Mesh007.geometry}
-              material={facialHairsMat}
-              position={[0.74, 2.87, 1.51]}
-              rotation={[1.42, -0.46, -0.97]}
-            />
-            <mesh
-              geometry={nodes.Mesh002.geometry}
-              material={facialHairsMat}
-              position={[0.71, 2.65, 1.47]}
-              rotation={[-1.21, -0.37, 0.93]}
-            />
-            <mesh
-              isMesh={() => console.log("ouilemesh")}
-              geometry={nodes.Mesh003.geometry}
-              material={facialHairsMat}
-              position={[-0.69, 2.64, 1.47]}
-              rotation={[-1.2, 0.4, -0.96]}
-            />
-            <mesh
-              geometry={nodes.Mesh001.geometry}
-              material={facialHairsMat}
-              position={[-0.69, 2.89, 1.52]}
-              rotation={[1.48, 0.34, 0.84]}
-            />
-            <mesh
-              geometry={nodes.Torus.geometry}
-              // material={materials.silver}
-              position={[1.4, 2, -0.01]}
-              rotation={[0, 0.39, -Math.PI / 2]}
-            />
-            <mesh
-              geometry={nodes.Bebar.geometry}
-              material={facialHairsMat}
-              position={[0, 3.25, 0]}
-              rotation={[Math.PI / 2, 0, 0]}
-            />
-            <mesh
-              geometry={nodes.Sphere1.geometry}
-              material={nodes.Sphere1.material}
-              position={[-0.62, 2.79, 1.25]}
-              rotation={[Math.PI / 2, 0, -0.03]}
-            />
-            <mesh
-              geometry={nodes.NurbsPath011.geometry}
-              material={materials.HairRoot}
-              position={[1.44, 2.7, -0.64]}
-              rotation={[-0.01, -0.04, 0.33]}
-            />
-            <mesh
-              geometry={nodes.NurbsPath.geometry}
-              material={materials.HairRoot}
-              position={[1.94, 4.89, 2.22]}
-            />
-            <mesh
-              geometry={nodes.Sphere1001.geometry}
-              material={nodes.Sphere1001.material}
-              position={[0.65, 2.79, 1.25]}
-              rotation={[Math.PI / 2, 0, 0]}
-            />
-            <mesh
-              geometry={nodes.Sphere2.geometry}
-              material={nodes.Sphere2.material}
-              position={[-0.62, 2.77, 1.14]}
-              rotation={[1.44, 0, 0]}
-            />
-            <mesh
-              geometry={nodes.Sphere2001.geometry}
-              material={nodes.Sphere2001.material}
-              position={[0.64, 2.78, 1.24]}
-              rotation={[1.44, 0, 0]}
-            />
-            <mesh
-              geometry={nodes.Head2001.geometry}
-              material={materials.Headus}
-              position={[0, 3.28, -0.05]}
-              rotation={[Math.PI / 2, 0, 0]}
-            />
-            <mesh
-              geometry={nodes.NurbsPath027.geometry}
-              material={materials.RootStrandLarge}
-              position={[0.83, 0.52, 0.02]}
-              rotation={[0.22, -0.17, -0.25]}
-            />
-            <mesh
-              geometry={nodes.NurbsPath001.geometry}
-              material={materials.RootStrandLarge}
-              position={[0.61, 4.16, -3.3]}
-              rotation={[0.78, -0.87, -1.86]}
-            />
-            <mesh
-              geometry={nodes.Broaux.geometry}
-              material={facialHairsMat}
-              position={[0, 3.23, -0.02]}
-              rotation={[Math.PI / 2, 0, 0]}
-            />
-            <group position={[0.25, 3.03, -1.37]} rotation={[1.29, 0.16, -1.24]}>
+    return (
+          <>
+            <group
+              {...props}
+              dispose={null}
+              // ref={headFull}
+              position={[5, 1.5, 9]}
+              rotation={[0.2, -0.8, 0]}
+            >
+            <spotLight
+      lookAt={[-12, 8, 2]}
+      position={[-4, -4, 14]}
+      intensity={5}
+      color="#ff00ff"
+      castShadow
+    />
+    <directionalLight intensity={0.51} position={[-2, 6, 10]} />
+
               <mesh
-                geometry={nodes.NurbsPath004_1.geometry}
+                geometry={nodes.Mesh007.geometry}
+                material={facialHairsMat}
+                position={[0.74, 2.87, 1.51]}
+                rotation={[1.42, -0.46, -0.97]}
+              />
+              <mesh
+                geometry={nodes.Mesh002.geometry}
+                material={facialHairsMat}
+                position={[0.71, 2.65, 1.47]}
+                rotation={[-1.21, -0.37, 0.93]}
+              />
+              <mesh
+                isMesh={() => console.log("ouilemesh")}
+                geometry={nodes.Mesh003.geometry}
+                material={facialHairsMat}
+                position={[-0.69, 2.64, 1.47]}
+                rotation={[-1.2, 0.4, -0.96]}
+              />
+              <mesh
+                geometry={nodes.Mesh001.geometry}
+                material={facialHairsMat}
+                position={[-0.69, 2.89, 1.52]}
+                rotation={[1.48, 0.34, 0.84]}
+              />
+              <mesh
+                geometry={nodes.Torus.geometry}
+                // material={materials.silver}
+                position={[1.4, 2, -0.01]}
+                rotation={[0, 0.39, -Math.PI / 2]}
+              />
+              <mesh
+                geometry={nodes.Bebar.geometry}
+                material={facialHairsMat}
+                position={[0, 3.25, 0]}
+                rotation={[Math.PI / 2, 0, 0]}
+              />
+              <mesh
+                geometry={nodes.Sphere1.geometry}
+                material={nodes.Sphere1.material}
+                position={[-0.62, 2.79, 1.25]}
+                rotation={[Math.PI / 2, 0, -0.03]}
+              />
+              <mesh
+                geometry={nodes.NurbsPath011.geometry}
                 material={materials.HairRoot}
+                position={[1.44, 2.7, -0.64]}
+                rotation={[-0.01, -0.04, 0.33]}
               />
               <mesh
-                geometry={nodes.NurbsPath004_2.geometry}
-                material={materials.hairStrands}
+                geometry={nodes.NurbsPath.geometry}
+                material={materials.HairRoot}
+                position={[1.94, 4.89, 2.22]}
               />
               <mesh
-                geometry={nodes.NurbsPath004_3.geometry}
+                geometry={nodes.Sphere1001.geometry}
+                material={nodes.Sphere1001.material}
+                position={[0.65, 2.79, 1.25]}
+                rotation={[Math.PI / 2, 0, 0]}
+              />
+              <mesh
+                geometry={nodes.Sphere2.geometry}
+                material={nodes.Sphere2.material}
+                position={[-0.62, 2.77, 1.14]}
+                rotation={[1.44, 0, 0]}
+              />
+              <mesh
+                geometry={nodes.Sphere2001.geometry}
+                material={nodes.Sphere2001.material}
+                position={[0.64, 2.78, 1.24]}
+                rotation={[1.44, 0, 0]}
+              />
+              <mesh
+                geometry={nodes.Head2001.geometry}
+                material={materials.Headus}
+                position={[0, 3.28, -0.05]}
+                rotation={[Math.PI / 2, 0, 0]}
+              />
+              <mesh
+                geometry={nodes.NurbsPath027.geometry}
                 material={materials.RootStrandLarge}
+                position={[0.83, 0.52, 0.02]}
+                rotation={[0.22, -0.17, -0.25]}
               />
-            </group>
-            <group position={[0.37, 3.09, 1.95]} rotation={[0.2, -1.04, 0.12]}>
               <mesh
-                geometry={nodes.NurbsPath031_1.geometry}
+                geometry={nodes.NurbsPath001.geometry}
                 material={materials.RootStrandLarge}
+                position={[0.61, 4.16, -3.3]}
+                rotation={[0.78, -0.87, -1.86]}
               />
               <mesh
-                geometry={nodes.NurbsPath031_2.geometry}
-                material={materials.hairStrands}
+                geometry={nodes.Broaux.geometry}
+                material={facialHairsMat}
+                position={[0, 3.23, -0.02]}
+                rotation={[Math.PI / 2, 0, 0]}
               />
+              <group position={[0.25, 3.03, -1.37]} rotation={[1.29, 0.16, -1.24]}>
+                <mesh
+                  geometry={nodes.NurbsPath004_1.geometry}
+                  material={materials.HairRoot}
+                />
+                <mesh
+                  geometry={nodes.NurbsPath004_2.geometry}
+                  material={materials.hairStrands}
+                />
+                <mesh
+                  geometry={nodes.NurbsPath004_3.geometry}
+                  material={materials.RootStrandLarge}
+                />
+              </group>
+              <group position={[0.37, 3.09, 1.95]} rotation={[0.2, -1.04, 0.12]}>
+                <mesh
+                  geometry={nodes.NurbsPath031_1.geometry}
+                  material={materials.RootStrandLarge}
+                />
+                <mesh
+                  geometry={nodes.NurbsPath031_2.geometry}
+                  material={materials.hairStrands}
+                />
+              </group>
             </group>
-          </group>
-        </>
-      );
-    }
-    useGLTF.preload("/headDefDISP.glb");
-      // useGLTF.preload("/headDefULTRA.glb");
-      // useGLTF.preload("/headDefLASH.glb");
+          </>
+        );
+      }
+      useGLTF.preload("/headDefDISP.glb");
